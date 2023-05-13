616c56ea52812d5535c00fc5aa99c43c4ce99180
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-05-13 14:39:37 +1200

    add pickup functionality

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,24 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    bool isFor3dTimer = false;
 
-    private void OnTriggerEnter (Collider other)
+
+    public void Start()
+    {
+        isFor3dTimer = true; // TODO: set using GameManager.is3d; 
+
+
+        Material m = GetComponent<Renderer>().material;
+        this.isFor3dTimer = isFor3dTimer;
+        // hacky change the colour of the pickup to red if it's for the 3d timer
+        if (isFor3dTimer)
+        {
+            m.color = Color.red;
+        }
+        else
+        {
+            m.color = Color.green;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Triggered!");
-        
-        if (other.gameObject.GetComponent<Obstacle>() != null) {
+
+        if (other.gameObject.GetComponent<Obstacle>() != null)
+        {
             // If i fucked up and placed the pickup inside an obstacle lol
             Destroy(gameObject);
         }
         // Check that the object we collided with is the player
-        else if (other.gameObject.CompareTag("Player")) {
+        else if (other.gameObject.CompareTag("Player"))
+        {
             // TODO: increase the time on the timer here
             // TODO: add particle/sound fx
+
+            if (isFor3dTimer)
+            {
+                // TODO: @nzkieran Start increase 3D timer event
+                Debug.Log("Increase 3d timer");
+            }
+            else
+            {
+                // TODO: @nzkieran Start increase 3D timer event
+                Debug.Log("Increase 2d timer");
+            }
+
             Destroy(gameObject);
         }
     }
-    
 }
